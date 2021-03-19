502fa984669aec7e96f41dabca4243c281629b89
haraphat01 <haraphat@gmail.com>
2021-03-19 21:40:55 +0100

    Added log

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,8 @@ class User < ApplicationRecord
                       has_secure_password
                       validates :password, presence: true, length: { minimum: 6 }
 
+                      def User.new_token
+                        SecureRandom.urlsafe_base64
+                      end
+
 end
