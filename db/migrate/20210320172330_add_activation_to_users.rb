d262da3f33eb388835f911f2f20cabd24a9ea9e5
haraphat01 <haraphat@gmail.com>
2021-03-22 09:47:36 +0100

    add

--- db/migrate/20210320172330_add_activation_to_users.rb
+++ db/migrate/20210320172330_add_activation_to_users.rb
@@ -1,5 +1,6 @@
 class AddActivationToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :activated, :boolean, default: false
+    add_column :users, :activated_at, :datetime
   end
 end
